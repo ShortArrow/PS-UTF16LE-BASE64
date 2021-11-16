94362e5aa7e3dac2775017d77e658a8835bc8634
ShortArrow <bamboogeneral@live.jp>
2021-11-16 13:16:16 +0900

    parameterize encoder

--- encoder.ps1
+++ encoder.ps1
@@ -1,7 +1,12 @@
-$srcFile = "payload.ps1"
-$dstFile = "payload.dat"
+param (
+    [string]$srcFile = "payload.ps1",
+    [string]$dstFile = "payload.dat",
+    [switch]$v
+)
 $srctext = Get-Content $srcFile
 $unicodeBytes = [System.Text.Encoding]::Unicode.GetBytes($srctext)
 $encoded = [System.Convert]::ToBase64String($unicodeBytes)
-$encoded
+if ($v) {
+    Write-Host $encoded
+}
 [System.Convert]::ToBase64String($unicodeBytes) | Out-File $dstFile -Encoding unicode
